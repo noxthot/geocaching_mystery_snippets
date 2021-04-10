7054c13dae0cc216bdc5816ed378d340a251a4fc
Gregor Ehrensperger <gregor.ehrensperger@gmx.com>
2021-04-10 23:56:58 +0200

    add prime

---
+++ prime/prime.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 10 20:51:41 2021
+
+@author: grego
+"""
+
+import sympy
+
+
+for nr in range(101, 1000):
+    if sympy.isprime(nr):                   # XYZ
+        if sympy.isprime(nr % 100):         # YZ
+            if sympy.isprime(nr // 10):     # XY
+                isPrime = True
+                    
+                for c in str(nr)
+                    isPrime &= sympy.isprime(int(c)) # X, Y, Z
+                    
+                if isPrime:
+                    print(nr)
