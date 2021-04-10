718506832ec61cd4189e52aed6dfa71ad79bef6f
Gregor Ehrensperger <gregor.ehrensperger@gmx.com>
2021-04-10 23:59:37 +0200

    Webscraper (looking for specific webpage content every X minutes)

---
+++ webscraper/main.py
@@ -0,0 +1,37 @@
+# This is a sample Python script.
+
+# Press Umschalt+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from datetime import datetime
+
+import os
+import requests
+import shutil
+import time
+
+url_root = 'https://www.muggelfrei.at/caches/schlaflos/'
+
+filenames = [f'N{idx}.png' for idx in range(1, 8)] + [f'E{idx}.png' for idx in range(1, 9)]
+
+
+if __name__ == '__main__':
+        while True:
+            now = datetime.now()
+            current_time = now.strftime("%H_%M")
+
+            try:
+                os.mkdir(current_time)
+            except Exception:
+                pass
+
+            for filename in filenames:
+                image_url = url_root + filename
+
+                r = requests.get(image_url, stream=True, verify=False)
+                r.raw.decode_content = True
+
+                with open(f'./{current_time}/{filename}', 'wb') as f:
+                    shutil.copyfileobj(r.raw, f)
+
+            time.sleep(30 * 60)  # Delay for 30 minutes
